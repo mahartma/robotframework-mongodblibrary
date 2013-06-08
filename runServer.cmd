63e2fa896efbd57c66dc66b5ca7beb676565c234
max.hartmann <max.hartmann@codecentric.de>
2013-06-09 01:04:14 +0200

    version changed

--- runServer.cmd
+++ runServer.cmd
@@ -1,2 +1,2 @@
 REM ======= starting Remote-Library =======
-java -jar build/libs/robotframework-mongodblibrary-0.2-with-dependencies.jar --port 8270
+java -jar build/libs/robotframework-mongodblibrary-0.2.1-with-dependencies.jar --port 8270
